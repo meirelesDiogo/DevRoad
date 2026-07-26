f5d01f31cf777eaf47f2ebf31a501d63042c66b5
Diogo Alexandre <didialexandre1017@gmail.com>
2026-07-26 19:14:33 -0300

    correcao de bug do metaData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,46 @@
-
+import type { Metadata } from "next";
 import "./globals.css";
-import  {Header}  from '../components/layout/header';
-import { Footer } from '../components/layout/footer';
 
-export const metadata:Metadata = {
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+
+export const metadata: Metadata = {
   title: "DevRoad - Aprenda a programar do zero",
-  description: "Roadmaps, aulas e projetos práticos para você sair do zero e chegar lá — sem pagar nada por isso.",
-openGraph: {
-  title: "DevRoad - Aprenda a programar do zero",
-  description: "Roadmaps, aulas e projetos práticos para você sair do zero e chegar lá — sem pagar nada por isso.",
-  images: ['https://dev-road-henna.vercel.app/logo.png'],
-}
- }
+  description:
+    "Roadmaps, aulas e projetos práticos para você sair do zero e chegar lá — sem pagar nada por isso.",
+
+  openGraph: {
+    title: "DevRoad - Aprenda a programar do zero",
+    description:
+      "Roadmaps, aulas e projetos práticos para você sair do zero e chegar lá — sem pagar nada por isso.",
+    images: [
+      {
+        url: "https://dev-road-henna.vercel.app/logo.png",
+        width: 512,
+        height: 512,
+        alt: "Logo DevRoad",
+      },
+    ],
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/logo.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en">
-      <body
-      className={`antialiased`}
-    >
- <Header/>
-      {children}
-      <Footer/>
+    <html lang="pt-BR">
+      <body className="antialiased">
+        <Header />
+        {children}
+        <Footer />
       </body>
     </html>
   );
