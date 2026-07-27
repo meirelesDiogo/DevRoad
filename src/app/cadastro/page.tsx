8ccd3e875ed120b6faf7a67fe2b4524c84fbc85d
Diogo Alexandre <didialexandre1017@gmail.com>
2026-07-27 18:49:01 -0300

    iniciado form de cadastro

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,13 @@
+
+export default function cadastro(){
+    return(
+        <div>
+            <h1>Cadastre-Se</h1>
+            <form method="POST">
+                <label htmlFor="">Nome:</label>
+                <input type="text"  />
+
+            </form>
+        </div>
+    );
+}
