052de79b8f09cbd5315a74214e0f96da77fee319
Diogo Alexandre <didialexandre1017@gmail.com>
2026-07-27 16:20:45 -0300

    configurando o auuth js

---
+++ src/app/login.tsx
@@ -0,0 +1,12 @@
+
+export default function login(){
+return(
+<div>
+    <h1>Login</h1>
+    <form method="POST">
+        <label>Nome:</label>
+        <input type="text" />
+    </form>
+</div>
+);
+};
