bf814561b68f2ea162adcc993d495356a79f373d
Diogo Alexandre <didialexandre1017@gmail.com>
2026-08-21 12:01:37 -0300

    limpando coonfig do Next

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    // Garante que a Vercel inclua os arquivos gerados pelo Prisma na build final
-    outputFileTracingIncludes: {
-      '/api/**/*': [path.resolve('./src/generated/prisma/**/*')],
-      '/*': [path.resolve('./src/generated/prisma/**/*')],
-    },
-  },
+  /*  build agora vai passar direto sem precisar rastrear arquivos manualmente */
 };
 
 export default nextConfig;
