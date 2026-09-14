ba0b6c50f4bc53b9eaaeb0fd23a3fbce5f8acf0d
Diogo Alexandre <didialexandre1017@gmail.com>
2026-09-14 20:43:13 -0300

    import pro roadmap

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,3 +1,27 @@
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from "@prisma/client";
+import pg from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalPrisma = global;
+const globalForPrisma = globalThis;
+
+const pool =
+  globalForPrisma.pgPool ||
+  new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.pgPool = pool;
+}
+
+const adapter = new PrismaPg(pool);
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
